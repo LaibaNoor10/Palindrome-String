7c4e9beb1ee128fc145ab83bd59a1e6c2f62c16c
LaibaNoor10 <125575046+LaibaNoor10@users.noreply.github.com>
2023-05-04 22:23:19 +0530

    Add files via upload

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to check whether it's palindrome or not :");
+            string str = Console.ReadLine();
+            string revString = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                revString += str[i];
+            }
+            if(str.ToLower() == revString.ToLower())
+            {
+                Console.WriteLine("Yes -- {0} is a palindrome string", revString);
+            }
+            else
+            {
+                Console.WriteLine("No -- {0} is not a palindrome string", revString);
+            }
+            Console.ReadLine();
+        }
+    }
+}
